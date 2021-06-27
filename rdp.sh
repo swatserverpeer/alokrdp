ef63f09787803aaea3dec45ee3709fe26b9b189f
swatserverpeer <46069093+swatserverpeer@users.noreply.github.com>
2021-06-27 16:19:59 +0530

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -4,5 +4,5 @@ sudo apt -y install xfce4
 sudo apt -y install firefox
 sudo apt install tightvncserver
 tightvncserver :1
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
+wget https://bin.equinox.io/a/8exBtGpBr59/ngrok-2.3.40-windows-amd64.zip
+unzip ngrok-2.3.40-windows-amd64.zip
